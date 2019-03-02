a0ea70bab2162a839e966dc649fb52ca108e6b32
Александр <fosswolfgang@hotmail.com>
2019-03-02 19:03:47 +0300

    Init
    Init

--- LabsIBKS/KGenerator/Main.c
+++ LabsIBKS/KGenerator/Main.c
@@ -3,21 +3,39 @@
 #include <locale.h>
 #include <time.h>
 #include <math.h>
-long long  m = 4294967296;
-long  a = 1664525;
-long  c = 1013904223;
+
+long long * Generate(size_t n) {
+	long long * items = (long long*)malloc(sizeof(long long)*n);
+	long long m = 4294967296;
+	long long a = 1664525;
+	long long c = 1013904223;
+	long long x0 = rand();
+	for (size_t i = 0; i < n; i++) {
+		long long  x = x0;
+		x = ((a * x) + c) % m;
+		x0 = x;
+		items[i] = x0;
+	}
+	return items;
+}
+
+void Print(long long *a, size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		printf("%llu\n", a[i]);
+	}
+}
+
+int GetPeriod(long long *a, size_t n) {
+	
+}
 
 int main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
 	printf("KGenerator.\n");
-	long long x0 = abs(rand());
-	for (int i = 0; i < 10000; i++) {
-		long long  x = x0;
-		x = ((a * x) + c) % m; 
-		x0 = x;
-		printf("%llu\n", x0);
-	}
+	int n = 100;
+	long long * items = Generate(n);
+
 	system("pause");
 	return 0;
 }
